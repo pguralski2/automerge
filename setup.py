3f42b78a940dc1d548d4c7ac298b1f2972980708
abenezer <abenezer@abenezers-MacBook-Air.local>
2021-12-10 09:26:31 -0800

    use make in workflows

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
+"""
+automerge
+-------------
+
+auto merge GitHub PRs
+"""
 from setuptools import setup, find_packages
-
-
-# read the contents of README file
 from os import path
 
-# get current file directory
 this_directory = path.abspath(path.dirname(__file__))
-# open README with UTF-8 encoding
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    # read README
     long_description = f.read()
 
 with open("requirements/prod.txt") as f:
